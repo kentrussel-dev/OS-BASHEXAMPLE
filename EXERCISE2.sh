ecea2f99cefd8620bf9fe291555fdbb57b21f8c7
kentrussel <109691713+kentrussel-dev@users.noreply.github.com>
2025-02-03 07:36:09 +0000

    exercise 2 and 3

---
+++ EXERCISE2.sh
@@ -0,0 +1,9 @@
+echo Enter your name:
+read yourname
+echo Your name is $yourname.
+
+echo Enter your age:
+read age
+echo Enter your birthdate: 
+read birthdate
+echo Your age is $age and your birthday is on $birthdate.
---
+++ EXERCISE3.sh
@@ -0,0 +1,25 @@
+let x=8
+let y=2
+
+let add=$x+$y
+echo $add
+let sub=$x-$y
+echo $sub
+let multiply=$x*$y
+echo $multiply
+let expo=$x**$y
+echo $expo
+let mod=$x%$y
+echo $mod
+let incrementby5=$x+5
+echo $incrementby5
+let decrementby5=$x-5
+echo $decrementby5
+let multiplyby5=$x*5
+echo $multiplyby5
+let divideby5=$x/5
+echo $divideby5
+let remainderby5=$x%5
+echo $remainderby5
+
+
